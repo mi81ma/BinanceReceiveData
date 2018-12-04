643ac44c29a24bc3915f5f5172529847955905bb
mi81ma <mi81ma@gmail.com>
2018-12-04 17:48:09 +0800

    Initial Commit

---
+++ BinanceReceiveData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinanceReceiveData
+//
+//  Created by masato on 4/12/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
